c79c36d2fa34e46c210d2c78437902dc52a70e65
prajapatdip <dipprajapat83@gmail.com>
2022-09-12 21:23:19 +0530

    main modified

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+    int a, b;
+    int c;
+    c = a + b;
+    std::cout << c << std::endl;
     std::cout << "Hello, world!" << std::endl;
     std::cin.get();
 }
